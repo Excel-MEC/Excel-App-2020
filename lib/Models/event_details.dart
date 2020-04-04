2bf21f963c08cab85620733c86fd0462c2fda1b3
Joyal A Johney <joyalajohny@gmail.com>
2020-04-04 15:53:59 +0530

    added Event details model

--- lib/Models/event_details.dart
+++ lib/Models/event_details.dart
@@ -1,2 +1,58 @@
 
-// Model for representing each Event
+// Model for representing each details of each Event
+
+class EventDetails {
+  int id;
+  String name;
+  String category;
+  String venue;
+  String prize;
+  String dateTime;
+
+  String about;
+  String format;
+  String rules;
+  List<dynamic> contacts;
+
+  EventDetails(
+      {this.id,
+      this.name,
+      this.category,
+      this.venue,
+      this.prize,
+      this.dateTime,
+      this.about,
+      this.format,
+      this.rules,
+      this.contacts});
+
+  factory EventDetails.fromJson(Map<String, dynamic> json) {
+    return EventDetails(
+        id: json['id'],
+        name: json['name'],
+        category: json['category'],
+        venue: json['venue'],
+        prize: json['prize_money'],
+        dateTime: json['date_time'],
+        about: json['about'],
+        format: json['format'],
+        rules: json['rules'],
+        // spelling mistake in api
+        contacts: json['conatcts']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return ({
+      'id': id,
+      'name': name,
+      'category': category,
+      'venue': venue,
+      'prize': prize,
+      'date': dateTime,
+      'about': about,
+      'format': format,
+      'rules': rules,
+      // no contacts added
+    });
+  }
+}
