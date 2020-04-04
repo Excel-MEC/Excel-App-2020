171c75c092cc3693f23de8c531ffc1c8a25ceae4
Joyal A Johney <joyalajohny@gmail.com>
2020-04-04 22:32:18 +0530

    method to add event details to table

--- lib/Services/Database/db_provider.dart
+++ lib/Services/Database/db_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:excelapp/Models/event_card.dart';
+import 'package:excelapp/Models/event_details.dart';
 import 'package:excelapp/Services/Database/Tables/events_table.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
@@ -26,15 +27,18 @@ class DBProvider {
     return ourDB;
   }
 
-  // Create tables in database
   void _onCreate(Database db, int version) async {
-    // TODO: execute all table creation as batch
+    // TODO: execute all table creation as batch - for single transaction
     await db.execute(DBEventsTable.createEventTable('Competitions'));
   }
 
+  void dispose() async {
+    final db = await database;
+    db.close();
+  }
 
   // Add multiple records(events) to table
-  addEvents(List<Event> events,String table) async {
+  addEvents(List<Event> events, String table) async {
     final db = await database;
     Batch batch = db.batch();
     for (var event in events) {
@@ -51,12 +55,23 @@ class DBProvider {
   // Retrieve all events from table
   Future<List<Event>> getEvents(String table) async {
     final db = await database;
-    final List<Map<String,dynamic>> res = await db.query(table);
+    final List<Map<String, dynamic>> res = await db.query(table);
     return res.map<Event>((row) => Event.fromJson(row)).toList();
   }
 
-  // TODO: Add one record using id
+  // add single event details to table
+  addEventDetails(EventDetails eventDetails,String table) async {
+    final db = await database;
+    await db.insert(table, eventDetails.toJson(),conflictAlgorithm: ConflictAlgorithm.replace);
+  }
 
-  // TODO: Retrieve one record using id
-  
+  // Retrieve one event details using id
+    Future<EventDetails> getEventDetails(String table,int id) async {
+    final db = await database;
+    List<Map<String,dynamic>> res = await db.query(table,where: 'id = ?',whereArgs: [id]);
+    Map<String,dynamic> event = res[0];
+    EventDetails eventDetails = EventDetails.fromJson(event);
+    return eventDetails;
+  }
+
 }
