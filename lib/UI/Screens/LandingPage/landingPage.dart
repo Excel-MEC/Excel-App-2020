ca4c7a749999d6afc3757edbddb6892dc198c724
Steev James <steevjames11@gmail.com>
2020-04-16 19:55:33 +0530

    Fixed swipe up returning to landing page issue

--- lib/UI/Screens/LandingPage/landingPage.dart
+++ lib/UI/Screens/LandingPage/landingPage.dart
@@ -17,27 +17,34 @@ class LandingPageState extends State<LandingPage> {
     screenSize = MediaQuery.of(context).size;
     _pageController = PageController(initialPage: 0);
 
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          backgroundImage(),
-          PageView(
-            controller: _pageController,
-            children: <Widget>[
-              GestureDetector( 
-                child: WelcomeCard(),
-                onTap: () {
-                  _pageController.animateToPage(1,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.easeInOut);
-                },
-              ),
-              // It redirects to the main page with the bottom navigation bar.
-              CustomNavigator()
-            ],
-            scrollDirection: Axis.vertical,
-          )
-        ],
+    return GestureDetector(
+      onVerticalDragStart: (_) {
+        _pageController.animateToPage(1,
+            duration: Duration(milliseconds: 1500), curve: Curves.easeInOut);
+      },
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            backgroundImage(),
+            PageView(
+              physics: NeverScrollableScrollPhysics(),
+              controller: _pageController,
+              children: <Widget>[
+                GestureDetector(
+                  child: WelcomeCard(),
+                  onTap: () {
+                    _pageController.animateToPage(1,
+                        duration: Duration(milliseconds: 1500),
+                        curve: Curves.easeInOut);
+                  },
+                ),
+                // It redirects to the main page with the bottom navigation bar.
+                CustomNavigator()
+              ],
+              scrollDirection: Axis.vertical,
+            )
+          ],
+        ),
       ),
     );
   }
