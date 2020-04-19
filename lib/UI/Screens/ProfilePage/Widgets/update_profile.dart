bc64448434cf7010c88e6e6c7eda5965317eafe8
Joyal A Johney <joyalajohny@gmail.com>
2020-04-19 11:32:55 +0530

    update profile form added

--- lib/UI/Screens/ProfilePage/Widgets/update_profile.dart
+++ lib/UI/Screens/ProfilePage/Widgets/update_profile.dart
@@ -15,13 +15,16 @@ class _UpdateProfileState extends State<UpdateProfile> {
   String _mobile;
   List<String> _categories = <String>['college', 'school', 'professional'];
   String _category = 'college';
+  List<String> _genders = <String>['Male', 'Female', 'Other'];
+  String _gender = 'Male';
   List<String> _institutions = <String>[
     'Harvard',
     'Model Engineering College',
     'Stanford',
     'MIT',
     'IIT Bombay',
-    'HArry Potter Uni'
+    'HArry Potter Uni',
+    'Other',
   ];
   String _institutionName = 'Model Engineering College';
 
@@ -35,100 +38,139 @@ class _UpdateProfileState extends State<UpdateProfile> {
         child: Form(
           key: _formKey,
           autovalidate: true,
-          child: Column(
-            children: <Widget>[
-              // Name
-              TextFormField(
-                onSaved: (String value) {
-                  setState(() {
-                    _name = value;
-                  });
-                },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return "Please enter your name";
-                  }
-                },
-                decoration: InputDecoration(
-                  hintText: 'Name',
-                  labelStyle: TextStyle(color: primaryColor),
-                ),
-              ),
-              // Mobile Number
-              TextFormField(
-                keyboardType: TextInputType.number,
-                onSaved: (String value) {
-                  setState(() {
-                    _mobile = value;
-                  });
-                },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return "Please enter your mobile number";
-                  }
-                },
-                decoration: InputDecoration(
-                  hintText: 'Mobile No:',
-                  labelStyle: TextStyle(color: primaryColor),
-                ),
-              ),
-              // Category
-              Row(
-                children: <Widget>[
-                  Text("Category: "),
-                  SizedBox(width: 10),
-                  DropdownButton<String>(
-                    value: _category,
-                    items: _categories.map<DropdownMenuItem<String>>((val) {
-                      return DropdownMenuItem<String>(
-                        value: val,
-                        child: Text(val),
-                      );
-                    }).toList(),
-                    onChanged: (String value) {
-                      setState(() {
-                        _category = value;
-                      });
-                    },
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                // Name
+                TextFormField(
+                  onSaved: (String value) {
+                    setState(() {
+                      _name = value;
+                    });
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Please enter your name";
+                    }
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Name',
+                    labelStyle: TextStyle(color: primaryColor),
                   ),
-                ],
-              ),
-              // Institutions
-              Row(
-                children: <Widget>[
-                  Text("Institutions: "),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: SearchableDropdown.single(
-                      value: _institutionName,
-                      items: _institutions.map<DropdownMenuItem<String>>((val) {
+                ),
+                SizedBox(height: 10),
+                // Mobile Number
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  onSaved: (String value) {
+                    setState(() {
+                      _mobile = value;
+                    });
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Please enter your mobile number";
+                    }
+                    if(value.length > 10) {
+                      return "Invalid Mobile number";
+                    }
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Mobile No:',
+                    labelStyle: TextStyle(color: primaryColor),
+                  ),
+                ),
+                SizedBox(height: 10),
+                // Select Category
+                Row(
+                  children: <Widget>[
+                    Text("Category: "),
+                    SizedBox(width: 10),
+                    DropdownButton<String>(
+                      value: _category,
+                      items: _categories.map<DropdownMenuItem<String>>((val) {
                         return DropdownMenuItem<String>(
                           value: val,
                           child: Text(val),
                         );
                       }).toList(),
-                      hint: 'Select Institution',
-                      searchHint: 'Enter here',
-                      onChanged: (value) {
+                      onChanged: (String value) {
                         setState(() {
-                          _institutionName = value;
+                          _category = value;
                         });
                       },
-                      isExpanded: true,
                     ),
-                  ),
-                ],
-              ),
-              RaisedButton(
-                child: Text("Submit"),
-                onPressed: () {
-                  _formKey.currentState.save();
-                  // TODO: It return whether form is valid or not. Therefore appropriate steps can be taken
-                  _formKey.currentState.validate();
-                },
-              ),
-              Text(_institutionName)
-            ],
+                  ],
+                ),
+                SizedBox(height: 20),
+                // Select Institution
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    // Note - Professionals
+                    Text("Note : Not applicable for professionals"),
+                    Row(
+                      children: <Widget>[
+                        // Text("Institution: "),
+                        // SizedBox(width: 10),
+                        Expanded(
+                          child: SearchableDropdown.single(
+                            value: _institutionName,
+                            items: _institutions
+                                .map<DropdownMenuItem<String>>((val) {
+                              return DropdownMenuItem<String>(
+                                value: val,
+                                child: Text(val),
+                              );
+                            }).toList(),
+                            hint: 'Select Institution',
+                            searchHint: 'Enter Institution Name',
+                            onChanged: (value) {
+                              setState(() {
+                                _institutionName = value;
+                              });
+                            },
+                            isExpanded: true,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+
+                // Select Gender
+                Row(
+                  children: <Widget>[
+                    Text("Gender: "),
+                    SizedBox(width: 10),
+                    DropdownButton<String>(
+                      value: _gender,
+                      items: _genders.map<DropdownMenuItem<String>>((val) {
+                        return DropdownMenuItem<String>(
+                          value: val,
+                          child: Text(val),
+                        );
+                      }).toList(),
+                      onChanged: (String value) {
+                        setState(() {
+                          _gender = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                RaisedButton(
+                  child: Text("Submit"),
+                  onPressed: () {
+                    _formKey.currentState.save();
+                    // TODO: It return whether form is valid or not. Therefore appropriate steps can be taken
+                    _formKey.currentState.validate();
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
