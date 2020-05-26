4787310894f6f125e98b4d74559c017ded12d301
Steev James <steevjames11@gmail.com>
2020-05-27 00:52:09 +0530

    Updated Schedule Page

--- lib/UI/Screens/Schedule/Widgets/generateScheduleCardList.dart
+++ lib/UI/Screens/Schedule/Widgets/generateScheduleCardList.dart
@@ -2,8 +2,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:excelapp/UI/constants.dart';
 import 'package:flutter/material.dart';
 
-List<Widget> eventList;
-
 class TimeTableList extends StatelessWidget {
   final List<Map<String, String>> eventDetails;
 
@@ -12,33 +10,27 @@ class TimeTableList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    addEventToList();
+    // addEventToList();
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[Padding(padding: EdgeInsets.all(8))] +
-          eventList +
+          List.generate(
+            eventDetails.length,
+            (i) => Event(
+                eventDetails[i]['name'],
+                eventDetails[i]['venue'],
+                eventDetails[i]['time'],
+                eventDetails[i]['image'],
+                i,
+                eventDetails.length),
+          ) +
           [
             SizedBox(
               height: 90,
             )
           ],
     );
-  }
-
-  addEventToList() {
-    eventList = List<Widget>();
-    for (int i = 0; i < eventDetails.length; i++) {
-      eventList.add(
-        Event(
-            eventDetails[i]['name'],
-            eventDetails[i]['venue'],
-            eventDetails[i]['time'],
-            eventDetails[i]['image'],
-            i,
-            eventDetails.length),
-      );
-    }
   }
 }
 
@@ -77,24 +69,29 @@ class EventState extends State<Event> {
       children: <Widget>[
         lineAndDot(lineNumber, eventLength),
         Expanded(
-          child: Card(
-            elevation: 2,
+          child: Container(
             margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
-            child: Container(
-                padding: EdgeInsets.fromLTRB(0, 7, 5, 7),
-                child: ListTile(
-                  title: Text(_eventName),
-                  leading: CachedNetworkImage(
-                    imageUrl: _imgurl,
-                    width: 40,
-                    height: 40,
-                  ),
-                  subtitle: Column(
-                    children: <Widget>[
-                      iconAndText(Icons.place, _venue),
-                      iconAndText(Icons.timer, _time)
-                    ],
-                  ),
+            child: ListTile(
+                dense: true,
+                title: Text(
+                  _eventName,
+                  style: TextStyle(
+                      fontFamily: pfontFamily,
+                      fontSize: 14.5,
+                      color: primaryColor),
+                ),
+                leading: CachedNetworkImage(
+                  imageUrl: _imgurl,
+                  width: 40,
+                  height: 40,
+                ),
+                subtitle: Text(
+                  _venue + '\n' + _time,
+                  style: TextStyle(
+                      color: primaryColor,
+                      fontSize: 11.0,
+                      decoration: TextDecoration.none,
+                      fontFamily: pfontFamily),
                 )),
           ),
         ),
@@ -103,41 +100,16 @@ class EventState extends State<Event> {
   }
 }
 
-Widget iconAndText(var icon, String text) {
-  var primaryColor = Color(0xff666688);
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.start,
-    children: <Widget>[
-      SizedBox(width: 10),
-      Icon(
-        icon,
-        size: 13.0,
-        color: primaryColor,
-      ),
-      SizedBox(width: 5),
-      Expanded(
-        child: Text(
-          text,
-          style: TextStyle(
-              color: primaryColor,
-              fontSize: 11.0,
-              decoration: TextDecoration.none,
-              fontFamily: pfontFamily),
-        ),
-      )
-    ],
-  );
-}
-
 Widget lineAndDot(lineNumber, noOfEvents) {
   noOfEvents = noOfEvents - 1;
-  double rowHeight = 93.0;
-  double left=23.0;
-  double circleRadius=4.2;
+  double rowHeight = 95.0;
+  double left = 25;
+  double circleRadius = 4.5;
+  double width = left * 2 - 10;
   return Stack(
     children: <Widget>[
       Container(
-        width: 45,
+        width: width,
       ),
       Container(
         margin: lineNumber != 0
@@ -149,12 +121,12 @@ Widget lineAndDot(lineNumber, noOfEvents) {
         height: lineNumber != 0
             ? lineNumber == noOfEvents ? rowHeight / 2 : rowHeight
             : rowHeight / 2,
-        color: Color(0xff777777),
+        color: primaryColor,
       ),
       Container(
-        margin: EdgeInsets.only(left: 19, top: 38),
+        margin: EdgeInsets.only(left: left - circleRadius, top: 38),
         child: CircleAvatar(
-          backgroundColor: lineNumber % 2 == 0 ? Colors.cyan : Colors.red,
+          backgroundColor: primaryColor,
           radius: circleRadius,
         ),
       )
