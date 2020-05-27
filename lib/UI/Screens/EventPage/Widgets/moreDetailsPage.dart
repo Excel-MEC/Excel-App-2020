f07ff78f10fc2ef76f9fc1bb4fd86919d2688d03
Joyal A Johney <joyalajohny@gmail.com>
2020-05-28 00:14:53 +0530

    event details bg updated

--- lib/UI/Screens/EventPage/Widgets/moreDetailsPage.dart
+++ lib/UI/Screens/EventPage/Widgets/moreDetailsPage.dart
@@ -25,7 +25,6 @@ class MoreDetailsState extends State<MoreDetails> {
         children: <Widget>[
           // Background Image
           getBackgroundImage(
-            eventDetails,
             [
               Color.fromRGBO(0, 0, 0, 0),
               Color.fromRGBO(23, 18, 41, .8),
