cb5e29f797db519dc19bbc4525f48eb7d494f38f
Steev James <steevjames11@gmail.com>
2020-06-16 18:00:44 +0530

    Changed login from auth0 to Oauth

--- lib/Accounts/auth_service.dart
+++ lib/Accounts/auth_service.dart
@@ -1,69 +1,52 @@
 import 'package:excelapp/Accounts/account_config.dart';
-import 'package:flutter_auth0/flutter_auth0.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
+import 'package:google_sign_in/google_sign_in.dart';
 import 'dart:convert';
 
-
 class AuthService {
-
   Future<String> login() async {
-
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    Auth0 auth0 = Auth0(
-      baseUrl: AccountConfig.baseUrl,
-      clientId: AccountConfig.clientId,
-    );
 
-    //Get access token from Auth0
+    // Get access token from Google
+    GoogleSignIn googleSignIn = GoogleSignIn(scopes: ['email']);
     String accessToken;
+    await googleSignIn.signOut();
+    GoogleSignInAccount accountInfo = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleKeys = await accountInfo.authentication;
+    accessToken = googleKeys.accessToken;
+
+    // Store access token locally
+    print("Access Token : $accessToken");
+    prefs.setString('access_token', accessToken);
+
+    // Send access token to backend -- Recieve jwt
     try {
-      var response = await auth0.webAuth.authorize({
-        'response_type' : 'token',
-        'client_id'     : AccountConfig.clientId,
-        'redirect_uri'  : AccountConfig.redirect,
-        'scope'         : 'openid profile email',
-      });
-
-      //store access token locally
-      accessToken = response['access_token'].toString();
-      print("Access Token : $accessToken");
-      prefs.setString('access_token', accessToken);
-    }
-    catch(e) {
-      print("Error: $e");
-    } 
-
-
-    //send access token to backend -- recieve jwt
-    try {
-      Map<String, String> token = {"auth_token": accessToken};
+      Map<String, String> token = {"accessToken": accessToken};
       var response = await http.post(
-        AccountConfig.url+'auth/login/',
+        AccountConfig.url + 'auth/login/',
         headers: {"Content-Type": "application/json"},
         body: json.encode(token),
       );
+      print(json.encode(token));
+      print(response.statusCode);
       final Map<String, dynamic> responseData = json.decode(response.body);
-      //store JWT token locally
+      // Store JWT token locally
       String jwt = responseData['token'].toString();
       print("JWT : $jwt");
       prefs.setString('jwt', jwt);
 
-      //User has logged in
+      // User has logged in
       prefs.setBool('isLogged', true);
-
-    }
-    catch(e) {
+    } catch (e) {
       print("Error: $e");
     }
     return 'success';
   }
 
- 
-
   Future<String> logout() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    //clear access token and jwt
+    // Clear access token and jwt
     await prefs.remove('access_token');
     await prefs.remove('jwt');
     await prefs.setBool('isProfileUpdated', false);
@@ -71,5 +54,4 @@ class AuthService {
 
     return 'success';
   }
-  
 }
