50b49cf712175e5d4b15daece0a4d6f01ddbdbff
Steev James <steev.james11@gmail.com>
2020-07-15 20:57:51 +0530

    Drawer completed

--- lib/UI/Screens/HomePage/Widgets/Drawer/drawer.dart
+++ lib/UI/Screens/HomePage/Widgets/Drawer/drawer.dart
@@ -1,66 +1,90 @@
 import 'package:excelapp/UI/Components/AlertDialog/alertDialog.dart';
-import 'package:excelapp/UI/Components/LoadingUI/alertDialog.dart';
 import 'package:excelapp/UI/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: ListView(
-                children: <Widget>[
-                  SafeArea(
-                    child: Center(),
-                    bottom: false,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                SafeArea(
+                  child: Center(),
+                  bottom: false,
+                ),
+                SizedBox(height: 10),
+                Center(
+                  child: Image.asset(
+                    "assets/excel logo.png",
+                    height: 110,
                   ),
-                  SizedBox(height: 20),
-                  Center(
-                    child: Image.asset(
-                      "assets/excel logo.png",
-                      width: 120,
-                    ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Excel 2020",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontSize: 21,
+                    fontFamily: pfontFamily,
                   ),
-                  SizedBox(height: 20),
-                  Text(
-                    "Excel 2020",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: primaryColor, fontSize: 22),
+                ),
+                Center(
+                  child: Container(
+                    color: Color(0x22000000),
+                    height: 1,
+                    width: 100,
+                    margin: EdgeInsets.symmetric(vertical: 15),
                   ),
-                  SizedBox(height: 20),
-                  Text(
-                    "Insipire  |  Innovate  |  Engineer",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: primaryColor,
-                      fontSize: 15,
-                      fontStyle: FontStyle.italic,
-                    ),
+                ),
+                Text(
+                  "Insipire  |  Innovate  |  Engineer",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontSize: 12,
+                    fontFamily: pfontFamily,
                   ),
-                  SizedBox(height: 40),
-                  DrawerOption(
-                    text: "Excel Prelims",
-                    icon: Icons.scatter_plot,
-                    onPressed: () {
-                      alertDialog(text: "Coming Soon", context: context);
-                    },
+                ),
+                Center(
+                  child: Container(
+                    color: Color(0x22000000),
+                    height: 1,
+                    width: 100,
+                    margin: EdgeInsets.symmetric(vertical: 15),
                   ),
-                  DrawerOption(
-                    text: "Excel Website",
-                    icon: Icons.web,
-                    onPressed: () {
-                      alertDialog(text: "Coming Soon", context: context);
-                    },
-                  ),
-                ],
-              ),
+                ),
+                SizedBox(height: 10),
+                DrawerOption(
+                  text: "Excel Prelims",
+                  icon: Icons.short_text,
+                  onPressed: () {
+                    alertDialog(text: "Coming Soon", context: context);
+                  },
+                ),
+                DrawerOption(
+                  text: "Excel Website",
+                  icon: Icons.dvr,
+                  onPressed: () {
+                    launch("https://excelmec.org/");
+                  },
+                ),
+                DrawerOption(
+                  text: "Share",
+                  icon: Icons.share,
+                  onPressed: () {
+                    alertDialog(text: "Coming Soon", context: context);
+                  },
+                ),
+              ],
             ),
-            SizedBox(height: 100)
-          ],
-        ),
+          ),
+          SizedBox(height: 100)
+        ],
       ),
     );
   }
@@ -76,25 +100,35 @@ class DrawerOption extends StatelessWidget {
     return FlatButton(
       onPressed: onPressed,
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 7),
         child: Row(
           children: <Widget>[
-            CircleAvatar(
-              backgroundColor: Color.fromRGBO(19, 145, 188, 1),
-              child: Icon(
-                icon,
-                color: Colors.white,
-              ),
+            Icon(
+              icon,
+              color: primaryColor,
+              size: 17,
             ),
-            SizedBox(width: 10),
+            SizedBox(width: 13),
             Text(
               text,
               textAlign: TextAlign.center,
-              style: TextStyle(color: Color(0xff555555), fontSize: 18),
+              style: TextStyle(
+                color: Color(0xaa000000),
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+launchURL(url) async {
+  if (await canLaunch(url) && url != '') {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
