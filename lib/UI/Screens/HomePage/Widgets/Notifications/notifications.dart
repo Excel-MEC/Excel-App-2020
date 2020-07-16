fedb1cea7dc54f0f2c12219168e7e1a1fedc61d0
Steev James <steev.james11@gmail.com>
2020-07-17 01:36:02 +0530

    Notification card made separate widget

--- lib/UI/Screens/HomePage/Widgets/Notifications/notifications.dart
+++ lib/UI/Screens/HomePage/Widgets/Notifications/notifications.dart
@@ -1,6 +1,6 @@
 import 'package:excelapp/UI/Components/Appbar/appbar.dart';
-import 'package:excelapp/UI/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:excelapp/UI/Screens/HomePage/Widgets/Notifications/notificationCard.dart';
 
 class NotificationsPage extends StatelessWidget {
   @override
@@ -12,55 +12,10 @@ class NotificationsPage extends StatelessWidget {
           children: <Widget>[SizedBox(height: 5)] +
               List.generate(
                 notificationData.length,
-                (index) => Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 5),
-                    child: Column(
-                      children: <Widget>[
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(Icons.notifications, color: Colors.grey),
-                            SizedBox(width: 10),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    notificationData[index]['title'],
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      color: primaryColor,
-                                    ),
-                                  ),
-                                  SizedBox(height: 5),
-                                  Text(
-                                    notificationData[index]["time"],
-                                    style: TextStyle(
-                                      color: Colors.blue,
-                                      fontSize: 11.5,
-                                    ),
-                                  ),
-                                  Divider(height: 10),
-                                  Text(
-                                    notificationData[index]['content'],
-                                    style: TextStyle(
-                                      color: Color(0x99000000),
-                                      fontSize: 12.5,
-                                      fontFamily: pfontFamily,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 5),
-                      ],
-                    ),
-                  ),
+                (index) => NotificationCard(
+                  title: notificationData[index]['title'],
+                  time: notificationData[index]['time'],
+                  description: notificationData[index]['content'],
                 ),
               ) +
               [SizedBox(height: 95)],
@@ -83,7 +38,7 @@ List<Map<String, dynamic>> notificationData = [
     "title": "Second notification",
     "time": "5 mins ago",
     "content":
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "* Lorem ipsum dolor sit amet\n* consectetur adipiscing elit\n* sed do eiusmod tempor incididunt ut.\n* labore et dolore magna aliqua.",
   },
   {
     "id": 3,
