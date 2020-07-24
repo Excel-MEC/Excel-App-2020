5e0a066231c29292f875fcdccb66d8526be86c95
Steev James <steev.james11@gmail.com>
2020-07-24 13:21:38 +0530

    Removed event head model

--- lib/Models/event_details.dart
+++ lib/Models/event_details.dart
@@ -135,28 +135,3 @@ class EventDetails {
     // return data;
   }
 }
-
-// class EventHead1 {
-//   int id;
-//   String name;
-//   String email;
-//   String phoneNumber;
-
-//   EventHead1({this.id, this.name, this.email, this.phoneNumber});
-
-//   EventHead1.fromJson(Map<String, dynamic> json) {
-//     id = json['id'];
-//     name = json['name'];
-//     email = json['email'];
-//     phoneNumber = json['phoneNumber'];
-//   }
-
-//   Map<String, dynamic> toJson() {
-//     final Map<String, dynamic> data = new Map<String, dynamic>();
-//     data['id'] = this.id;
-//     data['name'] = this.name;
-//     data['email'] = this.email;
-//     data['phoneNumber'] = this.phoneNumber;
-//     return data;
-//   }
-// }
