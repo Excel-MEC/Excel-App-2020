7d7cc83b6ca5173ee161377ed632b73733ba4dd6
Steev James <steev.james11@gmail.com>
2020-12-29 13:07:10 +0530

    Added local assets for categories

--- lib/UI/Screens/HomePage/Widgets/Categories/data.dart
+++ lib/UI/Screens/HomePage/Widgets/Categories/data.dart
@@ -2,31 +2,26 @@ var categoriesMap = [
   {
     "name": "Competitions",
     "info": "Its time to show who you are and what you are capable of.",
-    "imageUrl":
-        "https://images.unsplash.com/photo-1431324155629-1a6deb1dec8d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    "imageUrl": "assets/categories/competitions.jpg"
   },
   {
     "name": "Talks",
     "info": "Get inspired from the words of those, ready to spread it",
-    "imageUrl":
-        "https://images.unsplash.com/photo-1526398977052-654221a252b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+    "imageUrl": "assets/categories/talks.jpg"
   },
   {
     "name": "General",
     "info": "Dance to the songs, tap to the beats. Cause you gotta let it go.",
-    "imageUrl":
-        "https://images.unsplash.com/photo-1508253147813-f69206c47690?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+    "imageUrl": "assets/categories/general.jpg"
   },
   {
     "name": "Workshops",
     "info": "End up expertising in a skill by attending our workshops",
-    "imageUrl":
-        "https://images.unsplash.com/photo-1540575467063-178a50c2df87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    "imageUrl": "assets/categories/workshops.jpg"
   },
   {
     "name": "Conferences",
     "info": "Get inspired from the words of those, ready to spread it",
-    "imageUrl":
-        "https://images.unsplash.com/photo-1535016120720-40c646be5580?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    "imageUrl": "assets/categories/conferences.jpg"
   },
 ];
