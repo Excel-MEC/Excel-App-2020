9ab0dbc757ef1533e00a16202f2f88c4301ceaef
Steev James <steev.james11@gmail.com>
2021-01-01 19:12:32 +0530

    Trims input values in app

--- lib/UI/Screens/EventPage/Widgets/changeTeamPage.dart
+++ lib/UI/Screens/EventPage/Widgets/changeTeamPage.dart
@@ -9,6 +9,7 @@ import 'package:excelapp/UI/Components/Appbar/appbar.dart';
 import 'package:excelapp/UI/Components/LoadingUI/loadingAnimation.dart';
 import 'package:excelapp/UI/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ChangeTeamPage extends StatefulWidget {
   final EventDetails eventDetails;
@@ -121,6 +122,9 @@ class _ChangeTeamPageState extends State<ChangeTeamPage> {
                   key: _formKey,
                   child: TextFormField(
                     style: TextStyle(fontFamily: pfontFamily, fontSize: 15),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(RegExp("[0-9]"))
+                    ],
                     keyboardType: TextInputType.number,
                     onChanged: (String value) {
                       setState(() {
